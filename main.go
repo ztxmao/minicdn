69492fa911992b867d9c50ec3bf8fc04922204b8
chengjunxiong <chengjunxiong@chuangxin02v.add.corp.qihoo.net>
2018-10-17 10:58:50 +0800

    debug

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
@@ -38,8 +39,7 @@ var thumbNails = groupcache.NewGroup("thumbnail", 512<<20, groupcache.GetterFunc
 	}))
 
 func generateThumbnail(key string) ([]byte, error) {
-	u, _ := url.Parse(*mirror)
-	u.Path = key
+	u, _ := url.Parse(*mirror + key)
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return nil, err
@@ -55,9 +55,37 @@ func generateThumbnail(key string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func FileHandler(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Path
+func tunnel(r *http.Request) ([]byte, error) {
+	u, _ := url.Parse(*mirror)
+	u.Path = r.URL.Path
+	u.RawPath = r.URL.RawPath
+	u.RawQuery = r.URL.RawQuery
+	r.ParseForm()
+	req, err := http.NewRequest(r.Method, u.String(), strings.NewReader(r.Form.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	if *host != "" {
+		req.Host = *host
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Connection", "Keep-Alive")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
 
+func cacheTunnel(w http.ResponseWriter, r *http.Request) ([]byte, error) {
+	var key string
+	if *rule == "ignore" {
+		key = r.URL.Path
+	}
+	if *rule == "copy" {
+		key = r.URL.String()
+	}
 	state.addActiveDownload(1)
 	defer state.addActiveDownload(-1)
 
@@ -67,7 +95,7 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 			u.Path = r.URL.Path
 			u.RawQuery = r.URL.RawQuery
 			http.Redirect(w, r, u.String(), 302)
-			return
+			return nil, nil
 		}
 	}
 	fmt.Println("KEY:", key)
@@ -79,6 +107,19 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 		ctx.isFlush = true
 	}
 	err := thumbNails.Get(ctx, key, groupcache.AllocatingByteSliceSink(&data))
+	return data, err
+}
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	key := r.URL.Path
+	var data []byte
+	var err error
+	fmt.Println(method, *rule)
+	if method == "GET" && *rule != "proxy" {
+		data, err = cacheTunnel(w, r)
+	} else {
+		data, err = tunnel(r)
+	}
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -86,16 +127,23 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 	var modTime time.Time = time.Now()
 
 	rd := bytes.NewReader(data)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Cookie,Set-Cookie,x-requested-with,content-type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+	w.Header().Set("Via-Server", "goproxy cache")
 	http.ServeContent(w, r, filepath.Base(key), modTime, rd)
 }
 
 var (
-	mirror   = flag.String("mirror", "", "Mirror Web Base URL")
-	logfile  = flag.String("log", "-", "Set log file, default STDOUT")
-	upstream = flag.String("upstream", "", "Server base URL, conflict with -mirror")
-	address  = flag.String("addr", ":5000", "Listen address")
-	token    = flag.String("token", "1234567890ABCDEFG", "slave and master token should be same")
-	host     = flag.String("host", "", "server http host name")
+	mirror    = flag.String("mirror", "", "Mirror Web Base URL")
+	logfile   = flag.String("log", "-", "Set log file, default STDOUT")
+	upstream  = flag.String("upstream", "", "Server base URL, conflict with -mirror")
+	address   = flag.String("addr", ":5000", "Listen address")
+	token     = flag.String("token", "1234567890ABCDEFG", "slave and master token should be same")
+	host      = flag.String("host", "", "server http host name")
+	rule      = flag.String("rule", "ignore", "ignore:忽略URL参数；copy:遵循源站;proxy:透明穿透")
+	cachetime = flag.Int("t", 0, "缓存时间")
 )
 
 func InitSignal() {
